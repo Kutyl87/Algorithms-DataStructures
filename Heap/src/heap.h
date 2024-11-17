9fc8e52eb6f9a3497b7d8c2c00b0959ce0675ce0
pkutyl <01168894@pw.edu.pl>
2024-11-17 21:43:56 +0100

    Add heap

---
+++ Heap/src/heap.h
@@ -0,0 +1,23 @@
+#ifndef HEAP_H
+#define HEAP_H
+
+template <typename T>
+class Heap {
+private:
+  int arn;
+  std::vector<T> dataContainer;
+
+  std::size_t parent(std::size_t k) const { return (k - 1) / arn; }
+  std::size_t left(std::size_t k) const { return arn * k + 1; }
+  void up_heap(std::size_t k);
+  void down_heap(std::size_t k);
+
+public:
+  explicit Heap(int arnNumber) : arn(arnNumber) {}
+  T top() const { return dataContainer[0]; }
+  T pop();
+  void push(T element);
+};
+
+#include "heap.tpp"
+#endif
